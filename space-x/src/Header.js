9048d7b3bbb4d32de0bc75e295a0c6596ae2c33b
enjoy1818 <60070092@kmitl.ac.th>
2021-02-17 03:42:01 +0700

    Testing

--- space-x/src/Header.js
+++ space-x/src/Header.js
@@ -1,8 +1,19 @@
 
+import React, { useState, setState } from 'react'
 function Header() {
+    const [count, setCount] = useState(0);
+    const [dataCollection, setData ] = useState(0);
+    const axios = require('axios');
+    const itemCounts = axios.get('https://api.spacexdata.com/v3/landpads')
+    var items = Promise.resolve(itemCounts);
+    items.then(function (item) {
+        console.log(item[0])
+    })
     return (
         <div>
-            <h1>Space-X</h1>
+            <h1>You clicked {} times</h1>
+            <h1>{dataCollection}</h1>
+            <input onInput={(data) => console.log(data)}></input>
         </div>
     );
 }
