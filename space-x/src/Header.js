5789312104e7e850f5b9949db9b08571fb531adb
enjoy1818 <60070092@kmitl.ac.th>
2021-02-20 15:11:44 +0700

    Add launching data

--- space-x/src/Header.js
+++ space-x/src/Header.js
@@ -1,20 +1,51 @@
 
-import React, { useState, setState } from 'react'
-function Header() {
-    const [count, setCount] = useState(0);
-    const [dataCollection, setData ] = useState(0);
+import React, { useState, setState, useEffect } from 'react'
+import { Card } from 'antd'
+import './Header.css'
+function Header(props) {
+ 
+    const [dataCollection, setData] = useState([]);
     const axios = require('axios');
-    const itemCounts = axios.get('https://api.spacexdata.com/v3/landpads')
-    var items = Promise.resolve(itemCounts);
-    items.then(function (item) {
-        console.log(item[0])
-    })
-    return (
-        <div>
-            <h1>You clicked {} times</h1>
-            <h1>{dataCollection}</h1>
-            <input onInput={(data) => console.log(data)}></input>
+    useEffect(() => {
+        getData()
+    }, [])
+
+    async function getData() {
+    try{
+        const result = await axios.get('https://api.spacexdata.com/v3/launches')
+        var data = result.data
+        // console.log(data)
+        setData(data)
+    }
+        catch(err){
+            console.error(err)
+        }
+    }
+
+    function parseData(launchRecord){
+        // console.log(coreModule.length)
+        var coreSerial = launchRecord.rocket.first_stage.cores.map((core) =>
+            <p key={core.core_serial}>EngineID : {core.core_serial}</p>
+        );
+        // }
+        // console.log(typeof coreSerial)
+        return (
+        <div style={{display:"flex", flexDirection:"row"}}>
+            <div style={{flexGrow:"1"}}></div>
+            <div style={{flexGrow:"3"}}>
+            <Card key={launchRecord.flight_number} title={launchRecord.mission_name}>
+                <p key={launchRecord.rocket.rocket_name.id}>{launchRecord.rocket.rocket_name}</p>
+                {coreSerial}
+            </Card>
+            </div>
+            <div style={{flexGrow:"1"}}></div>
         </div>
+        )
+    }
+
+    return (
+        dataCollection.map(data => parseData(data))
+        
     );
 }
 export default Header;
